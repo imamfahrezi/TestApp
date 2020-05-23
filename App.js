3a216981f47392ad5d4b432b5b88979b1aa4914e
imamfahrezi <imamfahrezi24@gmail.com>
2020-05-23 18:34:45 +0700

    install react navigation and cara mengunakan react navigation perbedaan replace dan navigate pada react navigation adalah: replace tidak bisa kembali sedangkan navigate bisa

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, ScrollView} from 'react-native';
+import {View, ScrollView, Text} from 'react-native';
 import ContohComponents from './src/ContohComponents';
 import StyleBasic from './src/StyleBasic';
 import FlexBox from './src/FlexBox';
@@ -8,18 +8,18 @@ import StateDinamis from './src/StateDinamis';
 import Comunication from './src/comunications/Comunication';
 
 import {NavigationContainer} from '@react-navigation/native';
+import Router from './src/ojol/Router';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <ScrollView>
-        <ContohComponents />
-        <StyleBasic />
-        <FlexBox />
-        <PropsDinamis />
-        <StateDinamis />
-        <Comunication />
-      </ScrollView>
+      {/* <ContohComponents />
+     <StyleBasic />
+      <FlexBox />
+      <PropsDinamis />
+      <StateDinamis />
+      <Comunication /> */}
+      <Router />
     </NavigationContainer>
   );
 }
---
+++ src/ojol/Pages/Splash.js
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {View, Text} from 'react-native';
+
+const Splash = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Login');
+    }, 2000);
+  }, [navigation]);
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>HELLO SPLASH</Text>
+    </View>
+  );
+};
+
+export default Splash;
